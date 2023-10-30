309aaa23b97d34b398e414066e47c3e6a2749ad7
Muhammad Mohtasham <mmohtasham322@gmail.com>
2023-10-30 22:12:07 +0500

    updated

--- userside/doctor_registration.php
+++ userside/doctor_registration.php
@@ -1,5 +1,11 @@
 <?php 
 include("../admin/connection.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 $select_qualification = "SELECT * FROM `qualification`";
 $run_select_qualification = mysqli_query($connection, $select_qualification);
 $select_specialization = "SELECT * FROM `specialization`";
@@ -43,7 +49,25 @@ if($_SERVER["REQUEST_METHOD"]=== "POST"){
     move_uploaded_file($nic_back_image_tmp_name, $nic_back_image_path);
     $insert_doctor_query = "INSERT INTO `doctors`(`doctor_name`, `doctor_email`, `doctor_password`, `doctor_pic`, `doctor_exp`, `doctor_qualification`, `doctor_contact`, `doctor_degree_pic`, `doctor_nic_front_pic`, `doctor_nic_back_pic`, `doctor_city`, `doctor_whatsapp`, `doctor_gender`, `doctor_specialization`) VALUES ('$full_name','$email','$password ','$p_image_path','$experience','$qualification','$phone','$deg_image_path','$nic_front_image_path','$nic_back_image_path','$city','$whatsapp','$gender','$specialization')";
     $run_insert_doctor_query = mysqli_query($connection, $insert_doctor_query);
-    if($run_insert_doctor_query){
+    
+    $subject = "registration request on care website";
+    $txt = "Your registration request has been sent successfully and you will be responded within 24 working hours";
+    $mail = new PHPMailer(true);
+    $mail-> isSMTP();
+    $mail-> Host = 'SMTP.gmail.com';
+    $mail-> SMTPauth = true;
+    $mail-> Username = 'mmohtasham322@gmail.com';
+    $mail-> Password = 'eroll835...';
+    $mail-> SMTPSecure = 'ssl';
+    $mail-> Port = 465;
+    $mail-> setfrom("mmohtasham322@gmail.com");
+    $mail-> address($email);
+    $mail-> isHTML(true);
+    $mail-> subject = $subject;
+    $mail-> body = $txt;
+    $mail-> send();
+
+    if($run_insert_doctor_query && $mail){
       echo "<script>alert('query inserted successfully')</script>";
     }
     else{
